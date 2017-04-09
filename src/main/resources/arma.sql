e26f3faf59420a1f11606ce53e3e086f4d11cc8a
abzal <abzal_amanzhol_94@mail.ru>
2017-04-09 16:51:48 +0600

    Words.java

---
+++ src/main/resources/arma.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Апр 09 2017 г., 11:24
+-- Версия сервера: 10.1.21-MariaDB
+-- Версия PHP: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `arma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `alph_links`
+--
+
+CREATE TABLE `alph_links` (
+  `id` int(222) NOT NULL,
+  `link` text COLLATE utf8_unicode_ci NOT NULL,
+  `letter` varchar(3) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `alph_links`
+--
+
+INSERT INTO `alph_links` (`id`, `link`, `letter`) VALUES
+(1, 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/316222556&amp;', 'А а'),
+(2, 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/316239157&amp;', 'Ә ә'),
+(3, '', 'Б б'),
+(4, '', 'В в'),
+(5, '', 'Г г'),
+(6, '', 'Ғ ғ'),
+(7, '', 'Д д'),
+(8, '', 'Е е'),
+(9, '', 'Ё ё'),
+(10, '', 'Ж ж'),
+(11, '', 'З з'),
+(12, '', 'И и'),
+(13, '', 'Й й'),
+(14, '', 'К к'),
+(15, '', 'Қ қ'),
+(16, '', 'Л л'),
+(17, '', 'М м'),
+(18, '', 'Н н'),
+(19, '', 'Ң ң'),
+(20, '', 'О о'),
+(21, '', 'Ө ө'),
+(22, '', 'П п'),
+(23, '', 'Р р'),
+(24, '', 'С с'),
+(25, '', 'Т т'),
+(26, '', 'У у'),
+(27, '', 'Ұ ұ'),
+(28, '', 'Ү ү'),
+(29, '', 'Ф ф'),
+(30, '', 'Х х'),
+(31, '', 'Һ һ'),
+(32, '', 'Ц ц'),
+(33, '', 'Ч ч'),
+(34, '', 'Ш ш'),
+(35, '', 'Щ щ'),
+(36, '', 'ъ'),
+(37, '', 'Ы ы'),
+(38, '', 'І і'),
+(39, '', 'ь'),
+(40, '', 'Э э'),
+(41, '', 'Ю ю'),
+(42, '', 'Я я');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groupmembers`
+--
+
+CREATE TABLE `groupmembers` (
+  `G_NAME` varchar(255) NOT NULL,
+  `G_MEMBER` varchar(255) NOT NULL,
+  `ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `groupmembers`
+--
+
+INSERT INTO `groupmembers` (`G_NAME`, `G_MEMBER`, `ID`) VALUES
+('admin_role', 'admin', 88);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE `groups` (
+  `G_NAME` varchar(255) NOT NULL,
+  `G_DESCRIPTION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `groups`
+--
+
+INSERT INTO `groups` (`G_NAME`, `G_DESCRIPTION`) VALUES
+('admin_role', 'root - право'),
+('kitchen_role', 'кухня');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `test_type`
+--
+
+CREATE TABLE `test_type` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `test_type`
+--
+
+INSERT INTO `test_type` (`id`, `name`) VALUES
+('fillWords', 'Сөздерді толықтыр'),
+('other', 'Басқа сынақ'),
+('other2', 'Басқа сынақ 2');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `U_NAME` varchar(255) NOT NULL,
+  `U_PASSWORD` varchar(255) NOT NULL,
+  `U_DESCRIPTION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`U_NAME`, `U_PASSWORD`, `U_DESCRIPTION`) VALUES
+('admin', '64c5b12b7729e5076eaa577436042951', 's'),
+('kitchen', 'e10adc3949ba59abbe56e057f20f883e', ''),
+('mysql', 'e10adc3949ba59abbe56e057f20f883e', ''),
+('weblogic', 'e10adc3949ba59abbe56e057f20f883e', 'asd');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `U_NAME` varchar(255) NOT NULL,
+  `FIRSTNAME` varchar(255) DEFAULT NULL,
+  `LASTNAME` varchar(255) DEFAULT NULL,
+  `MIDDLENAME` varchar(255) DEFAULT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `LOCKED` int(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `user_detail`
+--
+
+INSERT INTO `user_detail` (`U_NAME`, `FIRSTNAME`, `LASTNAME`, `MIDDLENAME`, `EMAIL`, `LOCKED`) VALUES
+('admin', 'asd', 'asd', 'asdasd', 'abzal_amanzhol_94@mail.ru', 0),
+('kitchen', 'кухня', 'кухня', 'кухня', '', 0),
+('mysql', 'Test', 'Test', 'Test', '', 0),
+('weblogic', '????asdsa', '???dasd', '', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `words`
+--
+
+CREATE TABLE `words` (
+  `id` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `value_kz` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `words`
+--
+
+INSERT INTO `words` (`id`, `value_kz`) VALUES
+('05f8ba0e-c94f-42b4-80e3-b30cda8b4123', 'Сәлеметсізбе'),
+('15210d64-9a72-4d6f-9ec1-8c78ad845059', 'Жаз'),
+('2d5a8461-5565-49b5-88e8-43bfd1cdfa1e', 'Көктем'),
+('495bddd5-764f-4b29-9836-ad11f8d28d39', 'Күз'),
+('51e65471-8a3a-44e1-af7f-0bf75b9113c3', 'Әке'),
+('600dfe39-aab7-4ab5-8810-ed33dc84b50c', 'Қыс'),
+('74f013a5-f0e0-4637-9617-934452848a22', 'Қазақстан'),
+('a633fa76-e8f7-469f-afa8-efaddb3ae21d', 'Балапан'),
+('aa8a092b-97fc-41e8-8fc7-4800bf32d321', 'Мысық'),
+('afe09951-ae5c-40ad-9ecc-44a411146698', 'Қазақ'),
+('e9dc5b3a-dea7-42de-9b35-ee1798c96490', 'Әже'),
+('f3c8c6eb-6164-476f-8933-6cd67dcf0762', 'Ата'),
+('fc266d3a-35ee-47bc-9891-3d54e721fd60', 'Ана');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `alph_links`
+--
+ALTER TABLE `alph_links`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `test_type`
+--
+ALTER TABLE `test_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `words`
+--
+ALTER TABLE `words`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `alph_links`
+--
+ALTER TABLE `alph_links`
+  MODIFY `id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/main/webapp/js/valuation/fill-words.js
+++ src/main/webapp/js/valuation/fill-words.js
@@ -2,100 +2,125 @@ var charArray = [];
 
 function startFillWords() {
     get_ajax('/study/wr/app/getRandom10WordList', 'GET', null, function (gson) {
-        if (gson) {
-            $$("fillWordsContainer").removeView("fillWordsArena");
-            $$("fillWordsContainer").addView({
-                id: "fillWordsArena",
-                css: "fill-words-arena",
-                rows: []
-            });
-
-            var lastRand = 0;
-
-            $$("fillWordsArena").addView({
-                id: "fillWordsArenaCharsWrap",
-                rows: []
-            });
-
-            $$("fillWordsArena").addView({height: 30});
-            for (var i in gson) {
-                var word = gson[i].valueKz;
-                var ch = getRandomInt(1, word.length - 1);
-                if (lastRand == ch) {
-                    ch = getRandomInt(1, word.length - 1);
-                }
-                var left = word.substring(0, ch - 1);
-                var right = word.substring(ch);
-                charArray.push({id: word.substring(ch - 1, ch).toLowerCase()});
-
-                $$("fillWordsArena").addView({
-                    cols: [
-                        {
-                            view: 'text',
-                            id: gson[i].id + "Word",
-                            value: word,
-                            hidden: true
-                        },
-                        {
-                            view: "label",
-                            height: 40,
-                            autowidth: true,
-                            labelAlign: 'right',
-                            hidden: left.length == 0,
-                            id: gson[i].id + "Left",
-                            label: "<h3 style='margin-top: 0'>" + left + "</h3>"
-                        }, {
-                            view: 'text',
-                            width: 40,
-                            attributes: {maxlength: 1},
-                            id: gson[i].id,
-                            on: {
-                                onChange: function (newV, oldV) {
-                                    newV = newV.toLowerCase();
-                                    var me = this;
-                                    console.log(me)
-                                    var sel = getCharBySelected(charArray, this.config.id);
-                                    if (sel) {
-                                        sel.selected = null;
-                                    }
-
-                                    var obj = getCharById(charArray, newV);
-                                    if (obj) {
-                                        obj.selected = this.config.id;
-                                    } else {
-                                        //$$(this.config.id + "Required").define("label", "<p style='margin-top: 0;color: red;'>әріп " + newV + " табылмады</p>")
-                                        $$(this.config.id + "Required").define("label", "<p style='margin-top: 0;color: red;'>қате</p>")
-                                    }
-                                    setSelectedCharArray();
-                                }
-                            }
-                        },
-                        {
-                            view: "label",
-                            height: 40,
-                            autowidth: true,
-                            id: gson[i].id + "Right",
-                            label: "<h3 style='margin-top: 0'>" + right + "</h3>"
-                        },
-                        {
-                            view: "label",
-                            height: 40,
-                            ///autowidth: true,
-                            id: gson[i].id + "Required",
-                            label: ""
-                        }
-                    ]
-                });
-
-            }
-
-            charArraySort();
-            setSelectedCharArray()
-        }
-
+        createFillWords(gson);
     }, function (url) {
         messageBox("Ошибка", "Ошибка службы " + ' ' + url);
     });
+}
+
+function createFillWords(gson) {
+    if (gson) {
+        $$("fillWordsContainer").removeView("fillWordsArena");
+        $$("fillWordsContainer").addView({
+            id: "fillWordsArena",
+            css: "fill-words-arena",
+            rows: []
+        });
+
+        var lastRand = 0;
+
+        $$("fillWordsArena").addView({
+            id: "fillWordsArenaCharsWrap",
+            rows: []
+        });
+
+        $$("fillWordsArena").addView({height: 30});
+        for (var i in gson) {
+            var word = gson[i].valueKz;
+            var ch = getRandomInt(1, word.length - 1);
+            if (lastRand == ch) {
+                ch = getRandomInt(1, word.length - 1);
+            }
+            var left = word.substring(0, ch - 1);
+            var right = word.substring(ch);
+            charArray.push({id: word.substring(ch - 1, ch).toLowerCase()});
+
+            $$("fillWordsArena").addView({
+                cols: [
+                    {
+                        view: 'text',
+                        id: gson[i].id + "Word",
+                        value: word,
+                        hidden: true
+                    },
+                    {
+                        view: "label",
+                        height: 40,
+                        autowidth: true,
+                        labelAlign: 'right',
+                        hidden: left.length == 0,
+                        id: gson[i].id + "Left",
+                        label: "<h3 style='margin-top: 0'>" + left + "</h3>"
+                    }, {
+                        view: 'text',
+                        width: 40,
+                        attributes: {maxlength: 1},
+                        id: gson[i].id,
+                        on: {
+                            onChange: function (newV, oldV) {
+                                newV = newV.toLowerCase();
+                                var me = this;
+                                console.log(me)
+                                var sel = getCharBySelected(charArray, this.config.id);
+                                if (sel) {
+                                    sel.selected = null;
+                                }
+
+                                var obj = getCharById(charArray, newV);
+                                if (obj) {
+                                    obj.selected = this.config.id;
+                                    $$(this.config.id + "Required").hide()
+                                } else {
+                                    if (isNullOrEmpty(newV)) {
+                                        $$(this.config.id + "Required").hide();
+                                    } else {
+                                        $$(this.config.id + "Required").show();
+                                    }
+                                }
+                                setSelectedCharArray();
+                            }
+                        }
+                    },
+                    {
+                        view: "label",
+                        height: 40,
+                        autowidth: true,
+                        id: gson[i].id + "Right",
+                        label: "<h3 style='margin-top: 0'>" + right + "</h3>"
+                    },
+                    {
+                        view: "label",
+                        height: 40,
+                        width: 200,
+                        hidden: true,
+                        id: gson[i].id + "Required",
+                        label: "<p style='margin: 0;color: red'>Тізімде бар әріпті жазыңыз</p>"
+                    }
+                ]
+            });
+
+        }
+
+        charArraySort();
+        setSelectedCharArray();
+
+        $$("fillWordsContainer").removeView("fillWordsRequireBtnWrap");
+        $$("fillWordsContainer").addView({
+            id: "fillWordsRequireBtnWrap",
+            rows: [
+                {
+                    height: 30
+                },
+                {
+                    height: 50,
+                    id: "fillWordsRequireBtn",
+                    width: 155,
+                    css: "noBorder",
+                    template: "<button style='width: 145px;' class='btn btn-success'>Аяқтау</button>"
+                }
+            ]
+        });
+    }
 }
 
 function setSelectedCharArray() {
